f17c77109ef60b52cbabed6e02635fe4dc8cd9f7
rrahn <rene.rahn@fu-berlin.de>
2025-04-16 11:20:42 +0200

    Add sequence file input to global types

--- include/globaltypes.hpp
+++ include/globaltypes.hpp
@@ -3,9 +3,11 @@
 // SPDX-License-Identifier: CC0-1.0
 
 #include <libspm/seqan/alphabet.hpp>
-#include <libjst/rcms/compressed_multisequence.hpp> 
+#include <libjst/rcms/compressed_multisequence.hpp>
 #include <libjst/rcms/rcs_store.hpp>
-#include <libjst/coverage/int_coverage.hpp> 
+#include <libjst/coverage/int_coverage.hpp>
+
+#include <seqan3/io/sequence_file/input.hpp>
 
 using alphabet_t = spm::dna5;
 using coverage_t = libjst::int_coverage<uint32_t>;
@@ -13,3 +15,12 @@ using reference_t = std::vector<alphabet_t>;
 
 using cms_t = libjst::compressed_multisequence<reference_t, coverage_t>;
 using rcs_store_t = libjst::rcs_store<reference_t, cms_t>;
+
+struct sequence_input_traits : public seqan3::sequence_file_input_default_traits_dna
+{
+    using sequence_alphabet = alphabet_t;
+    using sequence_legal_alphabet = spm::dna15; // conversion?
+    static_assert(seqan3::explicitly_convertible_to<sequence_legal_alphabet, sequence_alphabet>);
+};
+
+using sequence_file_t = seqan3::sequence_file_input<sequence_input_traits>;
